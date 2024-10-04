b31ee0e5d655671f26d9cfcc757f8454421b670f
Rishab Chavadar <rchavadar@gmail.com>
2024-10-04 09:35:36 +0530

    Add files via upload

---
+++ Simple DMAS.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+
+    // Prompt the user to enter two integers
+    printf("Enter two integers: \n");
+    scanf("%d %d", &a, &b );
+
+    // Calculate the sum of the two integers
+    sum = a + b;
+
+    // Display the result
+    printf(" %d " , sum);
+
+    return 0;
+}
