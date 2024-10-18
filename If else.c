b455c18c50b004abee5e7d68f048de4e65517e1d
Rishab Chavadar <rchavadar@gmail.com>
2024-10-18 20:25:25 +0530

    Update If else.c

--- If else.c
+++ If else.c
@@ -2,17 +2,46 @@
 
 int main (){
 
-int age ;
-
-printf("Voteing Declaration\n") ;
+/*int age ;
+printf("Voting Declaration\n") ;
 printf("Enter your current age : ");
 scanf("%d" , &age) ;
-
 if(age >= 18) 
 printf("You are eligible to vote\n");
 else 
 printf("You are not eligible to vote");
+*/                                      //program to Check Voting age
 
+    
+/* int num ;
+print("Enter any integer Number : \n");
+scanf("%d" , &num);
+if(num%2==0)
+  printf("The entered number is Even");
+else
+  printf("The entered number is Odd");
+ */                                    //Program to check no is even or odd
+
+
+/* int a, b ;
+printf("Enter any 2 number");
+scanf("%d%d" , &a , &b);
+if(a>b)
+    printf("A is largest");
+else
+    printf("B is largest"); 
+*/                                    //Program to see which no is the largest
+
+
+/* char vowel ;
+printf("Enter any any alphabet : ");
+scanf("%c" , &char);
+if(vowel=='a' || vowel=='e' || vowel=='i' || vowel=='o' || vowel=='u')
+    printf("The alphabet is a vowel");
+else
+    printf("Not a vowel"); 
+*/                                    //Program to check whether letter is vowel
+        
 
 return 0 ;
 }
