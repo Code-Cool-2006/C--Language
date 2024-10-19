feb423a43f5ea80fc90e01fc3ee156ce397a24c8
Rishab Chavadar <rchavadar@gmail.com>
2024-10-19 12:36:00 +0530

    Add files via upload

---
+++ Nested If.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+int main()
+{
+
+/* int num ;
+printf("Number Identifier\n");
+printf("Enter a Valid Number : ");
+scanf("%d" , &num);
+
+if(num >= 0)
+     if(num > 0)
+     printf("The entered number is Positive\n");
+
+     else
+     printf("The number entered is Zero\n");
+
+else
+printf("The number entered is Negative\n");
+ */                                               //Program to identify +ve, -ve numbers and zero  
+
+
+ /* int a, b, c ;
+printf("Welcome to the largest value finder\n");
+printf("Enter 1st Number : ");
+scanf("%d" , &a);
+printf("Enter 2nd Number : ");
+scanf("%d" , &b);
+printf("Enter 3rd Number : ");
+scanf("%d" , &c);
+
+if(a>b)
+     if(a>c)
+     printf("%d is largest\n" , a);
+     else
+     printf("%d is largest\n" ,c);
+else
+     if(b>c)
+     printf("%d is largest\n" , b);
+     else
+     printf("%d is largest\n" , c);
+*/                                               //Program to find largest of 3 numbers
+
+
+/* int x, y ;
+printf("Welcome to Quadrant Finder\n");
+printf("Enter the value of x  : ");
+scanf("%d" , &x);
+printf("Enter the value of y  : ");
+scanf("%d" , &y);
+
+
+if(x>0)
+     if(y>0)
+     printf("The point lies in 1st Quadrant");
+     else
+     printf("The point lies in 4th Quadrant");
+else
+     if(y>0)
+     printf("The point lies in 2nd Quadrant");
+     else
+     printf("The point lies in 4th Quadrant");
+*/                                               //Program to find Quadrant of the given Point
+
+
+     return 0 ;
+}
+
+
